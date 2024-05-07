bd54923b15e64b184e328213baea3d5785270302
Ryuketsukami <Ryuketsukami@gmail.com>
2024-05-07 12:59:45 +0300

    commit

--- back_project/urls.py
+++ back_project/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import hello_geeks
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', hello_geeks),
 
     # Allan mapping
     path('', include("messages_app.urls")),
