f3bb5fdf1b0bec8499c257b6a6acf27b55c8401d
Ryuketsukami <Ryuketsukami@gmail.com>
2024-05-08 19:04:49 +0300

    user

--- authentication_app/urls.py
+++ authentication_app/urls.py
@@ -5,5 +5,5 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('register/', RegisterView.as_view(), name='auth_register', method=['post']),
+    path('register/', RegisterView.as_view(), name='auth_register'),
 ]
