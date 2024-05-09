c2807218f886078f404bd00329ca45a3dfcb7221
Ryuketsukami <Ryuketsukami@gmail.com>
2024-05-09 16:23:31 +0300

    views clean

--- back_project/views.py
+++ back_project/views.py
@@ -1,79 +1,11 @@
-# added by Allan
-
-# HttpResponse is used to
-# pass the information 
-# back to view
 from django.http import HttpResponse
 from django.http import JsonResponse
 from django.contrib.auth.models import User
-from rest_framework import generics
 from rest_framework.permissions import AllowAny
-from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.authtoken.models import Token
-from rest_framework.response import Response
 from rest_framework.views import APIView
-import json
  
-# Defining a function which
-# will receive request and
-# perform task depending 
-# upon function definition
+
 def render_home (request) :
 
     # string as HttpResponse
     return HttpResponse("This is the home page!")
-
-
-
-def create_user (request):
-
-    if request.method != 'POST':
-        return JsonResponse({'error': 'Invalid request method'}, status=405)
-
-    data = json.loads(request.body)
-
-    if not data:
-        return JsonResponse({'error': 'Missing parameters in body'}, status=400)
-
-    username = data.get('username')
-    password = data.get('password')
-    email = data.get('email')
-
-    if not username or not password or not email:
-        return JsonResponse({'error': 'Missing parameters in body'}, status=400)
-    
-    try:
-        User.objects.create_user(
-            username=username,
-            email=email,
-            password=password
-        )
-        return JsonResponse({'message': 'User successfully created!'})
-    except Exception as e:
-        return JsonResponse({'error': str(e)}, status=500)
-    
-
-class CreateUserView(APIView):
-    permission_classes = (AllowAny,)
-
-    def post(self, request):
-        data = request.data
-        if not data:
-            return JsonResponse({'error': 'Missing parameters in body'}, status=400)
-
-        username = data.get('username')
-        password = data.get('password')
-        email = data.get('email')
-
-        if not username or not password or not email:
-            return JsonResponse({'error': 'Missing parameters in body'}, status=400)
-
-        try:
-            User.objects.create_user(
-                username=username,
-                email=email,
-                password=password
-            )
-            return JsonResponse({'message': 'User successfully created!'})
-        except Exception as e:
-            return JsonResponse({'error': str(e)}, status=500)
